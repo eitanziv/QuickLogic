0b84552f9cf29451acf0af6a7549f61a2ef9c63b
QuickDrawSnoops <will.49591m@gmail.com>
2026-05-01 22:34:15 -0400

    Worked on the Logic Analyzer and added the function of breaking down the .sr file into bytes and checking to see which channels are active and not active

--- Logic_Analyze.py
+++ Logic_Analyze.py
@@ -12,13 +12,41 @@ else:
 	print("Issues reading capture file please check!")
 	sys.exit()
 
+## reads .sr file and grabs the logic files that will be analyzed
+raw_data = bytearray()
 
 with zipfile.ZipFile(Capture_File, 'r') as capture:
 	files = capture.namelist()
 	df = [f for f in files if f.startswith("logic-1")]
 	print(df)
 
+## breaks down the logic files into one byte array
+	for data_files in df:
+		full_bytes = capture.read(data_files)
+		raw_data.extend(full_bytes)
+	with open("bytes.txt", "wb") as byte_file:  ## This needs to be removed in function version since everything will be in memory
+		byte_file.write(raw_data)
 
+## This section performs a check for each channel to see if the channel idles high or low indicating no traffic and adds the active channels to an array
+channels = [0, 1, 2, 3, 4, 5, 6, 7]
+channel_data = {}
+active_channels = []
+
+for channel in channels:
+	bits = []
+	for byte in raw_data:
+		bit = (byte >> channel) & 1
+		bits.append(bit)
+	channel_data[channel] = bits
+
+for channel, bits in channel_data.items():
+	if len(set(bits)) == 1:
+		print(f"Nothing going on at channel {channel}")
+	else:
+		active_channels.append(channel)
+		print(f"Active channel on channel {channel}")
+
+## Beginning of the analysis functions
 
 
 

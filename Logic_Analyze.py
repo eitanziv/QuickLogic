31360bf229424005a37c653f7c5235bf26f5f30e
QuickDrawSnoops <will.49591m@gmail.com>
2026-05-05 00:11:40 -0400

    Worked on the Logic Analyzer and tested the function of searching the bits to read the bytes. Reorginized the codeblock and setup the UART_Check function to prepaer for the actual analyzing.

--- Logic_Analyze.py
+++ Logic_Analyze.py
@@ -1,9 +1,21 @@
 import zipfile
 import sys
 
+COMMON_BAUDS = [115200, 9600, 38400, 57600, 19200, 4800]
+SAMPLE_RATE = 8_000_000
+CHANNELS = [0, 1, 2, 3, 4, 5, 6, 7]
+
+channel_data = {}
+active_CHANNELS = []
+##samples_per_bit = SAMPLE_RATE / COMMON_BAUDS[0] //This will go in the UART_Check function
+
+## This is the list of functions
+## Check for UART function
+def UART_Check (bits, SAMPLE_RATE, Capture_File):
 
 
-##Grabs .sr file anc checks magic bytes to see if it is a correct zip
+
+##Grabs .sr file anc checks magic bytes to see if it is a correct zip THIS WILL BE CHANGED AND WILL BE FROM THE COMMANDS PARAMETERS
 Capture_File = "test_capture.sr"
 
 if zipfile.is_zipfile(Capture_File):
@@ -24,15 +36,9 @@ with zipfile.ZipFile(Capture_File, 'r') as capture:
 	for data_files in df:
 		full_bytes = capture.read(data_files)
 		raw_data.extend(full_bytes)
-	with open("bytes.txt", "wb") as byte_file:  ## This needs to be removed in function version since everything will be in memory
-		byte_file.write(raw_data)
 
 ## This section performs a check for each channel to see if the channel idles high or low indicating no traffic and adds the active channels to an array
-channels = [0, 1, 2, 3, 4, 5, 6, 7]
-channel_data = {}
-active_channels = []
-
-for channel in channels:
+for channel in CHANNELS:
 	bits = []
 	for byte in raw_data:
 		bit = (byte >> channel) & 1
@@ -43,15 +49,6 @@ for channel, bits in channel_data.items():
 	if len(set(bits)) == 1:
 		print(f"Nothing going on at channel {channel}")
 	else:
-		active_channels.append(channel)
+		active_CHANNELS.append(channel)
 		print(f"Active channel on channel {channel}")
 
-## Beginning of the analysis functions
-
-
-
-
-
-
-## This is a list of common baud rates to check. Future versions will offer an option to calculate the baud if these do not work.
-##Common_Bauds = [115200, 9600, 38400, 57600, 19200, 4800]
